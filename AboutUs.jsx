b7afc2d862a2bbc641e02bdfb852a95dc8bb7ef6
hectorsep2008-lab <hectorsep2008@gmail.com>
2026-06-02 20:26:08 +0200

    Enhance AboutUs component with title and description
    Added application description and title to AboutUs component.

--- AboutUs.jsx
+++ AboutUs.jsx
@@ -1,1 +1,3 @@
+# Paradise Nursery Shopping Application
 
+Paradise Nursery is an online plant shop that allows users to browse a variety of houseplants, add them to a shopping cart, and manage their cart items. The application is built using React and Redux Toolkit.
